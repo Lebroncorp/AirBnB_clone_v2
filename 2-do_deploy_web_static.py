baf9c37fb2eb83d17963d9ca495f9377c70d28fa
Lebroncorp <lebroncorp@yahoo.com>
2024-01-11 18:56:56 -0800

    uploads

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,7 +9,7 @@ import os
 from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
-env.hosts = ["34.138.32.248", "3.226.74.205"]
+env.hosts = ["54.90.18.48", "54.90.32.85"]
 
 
 def do_deploy(archive_path):
